a932a339eb4a2935a799a0a9b378ff1cf98aae26
KevinW <bird831205@hotmail.com>
2020-04-04 14:32:54 -0400

    iterating thorugh files

--- readFiles.py
+++ readFiles.py
@@ -10,3 +10,17 @@ file.close()
 
 with open("text.txt") as file:
     print(file.read())
+
+print("\n============")
+with open("text.txt") as file:
+    for line in file:
+        print(line.strip().upper())
+
+print("\n============")
+file = open("text.txt")
+lines = file.readlines()
+file.close()
+
+for line in lines:
+    line.capitalize()
+print(lines)
