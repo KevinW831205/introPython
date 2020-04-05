19cc9fbb734b4a1c7e349a187d4c4160168b1f48
KevinW <bird831205@hotmail.com>
2020-04-04 22:19:55 -0400

    quantifiers

--- regex.py
+++ regex.py
@@ -22,3 +22,11 @@ print(re.search(r"[^a-zA-Z! ]","asd g"))
 print(re.search(r"cat|dog","somecat"))
 print(re.search(r"cat|dog","somedog"))
 print(re.findall(r"cat|dog","somedog somecat somdog"))
+print(re.search(r"ab.*n","abn"))
+print(re.search(r"ab.*n","abogn"))
+print(re.search(r"ab.*n","abognin"))
+print(re.search(r"o+l+","goldfish"));
+print(re.search(r"o+l+","gooooldfish"));
+print(re.search(r"o+l+","goooodlfish"));
+print(re.search(r"ab?a","aaa"))
+print(re.search(r"ab?a","abaa"))
