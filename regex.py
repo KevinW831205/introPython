c677e0781805620b2dbbf1a881094947bb59af35
KevinW <bird831205@hotmail.com>
2020-04-04 22:04:33 -0400

    more regex

--- regex.py
+++ regex.py
@@ -12,3 +12,13 @@ print(result)
 print(re.search(r"p.ng","penguin"))
 print(re.search(r"p.ng","ping"))
 print(re.search(r"p.ng","pinng"))
+
+
+print(re.search("[a-z]way","away the highway"))
+print(re.search("cloud[a-zA-Z0-9]","cloudy"))
+print(re.search("cloud[a-zA-Z0-9]","cloud9"))
+print(re.search("cloud[a-zA-Z0-9]","cloud!"))
+print(re.search(r"[^a-zA-Z! ]","asd g"))
+print(re.search(r"cat|dog","somecat"))
+print(re.search(r"cat|dog","somedog"))
+print(re.findall(r"cat|dog","somedog somecat somdog"))
