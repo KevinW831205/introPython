b326879f70f268a3d3f97e4feccaf43beec39f75
KevinW <bird831205@hotmail.com>
2020-04-06 18:01:30 -0400

    wrapup

---
+++ dataStream.py
@@ -0,0 +1,16 @@
+import subprocess
+import os
+
+# name = input("please enter your name")
+# print("your name is "+name)
+
+
+
+#
+# result = subprocess.run(["host","8.8.8.8"],capture_output=True,shell=True)
+# print(result.stderr.decode().split())
+#
+# my_env = os.environ.copy()
+# my_env["PATH"] = os.pathsep.join(["/opt/myapp/",my_env["PATH"]])
+#
+# result = subprocess.run(["myapp"], env=my_env)
