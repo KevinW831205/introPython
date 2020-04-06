48264f94c145123a5b8736e168762e094599e09c
KevinW <bird831205@hotmail.com>
2020-04-06 18:13:32 -0400

    edge case test

--- adding.py
+++ adding.py
@@ -1,2 +1,4 @@
 def add(num1,num2):
+    if num1 is None or num2 is None:
+        return None
     return num1 + num2
--- adding_test.py
+++ adding_test.py
@@ -6,7 +6,8 @@ class MyTestCase(unittest.TestCase):
         testcase = (1,2)
         expected = 3;
         self.assertEqual(add(testcase[0],testcase[1]), expected)
-
+    def test_None(self):
+        self.assertEqual(add(None, None),None)
 
 if __name__ == '__main__':
     unittest.main()
