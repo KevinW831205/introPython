f39e88644a4ef9e53727e0b98f3de6bf6e4196b9
KevinW <bird831205@hotmail.com>
2020-04-06 20:48:59 -0400

    more error handling

--- try-except.py
+++ try-except.py
@@ -3,3 +3,13 @@ def open_file(filename):
         f= open(filename)
     except OSError:
         return None
+
+def validate_length(username, minlen):
+    assert type(username) == str, "username must be string"
+    if minlen <1:
+        raise ValueError("minlen must be atleast 1")
+    if len(username)<5:
+        return False
+    return True
+
+validate_length(5,5)
