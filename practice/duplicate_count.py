62a730b8734469e7d656ba8eff291f1ac6bf9bfd
KevinW <bird831205@hotmail.com>
2020-04-08 23:06:43 -0400

    changed function to count duplicate by letter

--- practice/duplicate_count.py
+++ practice/duplicate_count.py
@@ -1,9 +1,10 @@
 def duplicate_count(text):
     duplicates = set()
     result = 0;
+
     for letter in text:
-        if letter in duplicates:
-            result += 1;
-        else:
-            duplicates.add(letter)
+        if text.count(letter) > 1 and not letter in duplicates:
+            duplicates.add(letter);
+            result +=1
+
     return result
