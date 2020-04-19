cf4abd2e48da2ab33099654d7ccd4f29149aa1db
KevinW <bird831205@hotmail.com>
2020-04-19 14:51:53 -0400

    making a post request with data

--- webapps/run.py
+++ webapps/run.py
@@ -1,11 +1,13 @@
 import os
 import requests
+import json
 
 os.listdir()
 ip = ""
+feedback_dir = ""
 
 def get_feedback():
-    return os.listdir("/data/feedback")
+    return os.listdir(feedback_dir)
 
 def parse_file(file_dir):
     with open(file_dir, "r") as file:
@@ -16,3 +18,11 @@ def parse_file(file_dir):
        feedback_dictionary["feedback"] = file.readline()
        return feedback_dictionary;
 
+def run():
+    files = get_feedback();
+    for file in files:
+        data = parse_file(feedback_dir+"/"+feedback_dir)
+        data_json = json.dump(data,indent=2)
+
+        response = requests.post(ip+"/feedback", data=data_json)
+        print(response.status_code + response.text)
