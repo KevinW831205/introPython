4ac33504047954d6208e03e66c68bf26cecc9767
KevinW <bird831205@hotmail.com>
2020-04-20 16:31:16 -0400

    attatchments

--- emails/intro.py
+++ emails/intro.py
@@ -19,4 +19,13 @@ message.set_content(body)
 attatchment_path = "sendme.txt"
 attatchment_filename = os.path.basename(attatchment_path)
 attatchement_mime_type,_ = mimetypes.guess_type(attatchment_path)
-print(attatchement_mime_type)
+mime_type, mime_subtype = attatchement_mime_type.split('/',1)
+
+
+with open(attatchment_path,'rb') as ap:
+    message.add_attachment(ap.read(),
+                           maintype = mime_type,
+                           subtype = mime_subtype,
+                           filename = os.path.basename(attatchment_path))
+
+print(message)
