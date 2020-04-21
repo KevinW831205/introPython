db9b0d8f174325d9eb8e83cbfb84fd2dec9712f2
KevinW <bird831205@hotmail.com>
2020-04-21 14:45:25 -0400

    upload image

--- general/supplier_image_upload.py
+++ general/supplier_image_upload.py
@@ -13,13 +13,20 @@ def is_jpeg(filename):
     except:
         return False
 
+def upload(filename):
+    with open(img_directory+"/"+filename,'rb') as opened:
+        r = requests.post(url,files={'file':opened})
+        print("uploading {} to {} responded with {}".format(filename,url,r.status_code))
 
-with open('/usr/share/apache2/icons/icon.sheet.png', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
+# with open('/usr/share/apache2/icons/icon.sheet.png', 'rb') as opened:
+#     r = requests.post(url, files={'file': opened})
+
 
 def __main__():
     for file in os.listdir(img_directory):
-        print(file)
+        if is_jpeg(file):
+            upload(file)
+
 
 if __name__ == '__main__':
     __main__()
