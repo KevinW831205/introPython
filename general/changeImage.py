055f2c18e9fbe7b82c933b366e53a2a138164aaa
KevinW <bird831205@hotmail.com>
2020-04-21 14:11:52 -0400

    modified the process image code

---
+++ general/changeImage.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os
+
+def process_image(dir):
+    for file in os.listdir(dir):
+        print(file)
+        try:
+            img = Image.open(file)
+        except:
+            print("Error opening Image")
+            continue
+        img.resize((600, 400)).convert('RGB').save("../opt/icons/" + file, "JPEG")
+
+
+process_image("~/supplier-data/images")
