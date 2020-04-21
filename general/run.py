5b1929fc360984621100cec2c8989f5a406a87fc
KevinW <bird831205@hotmail.com>
2020-04-21 15:11:19 -0400

    properly assign imagename

--- general/run.py
+++ general/run.py
@@ -5,19 +5,19 @@ import requests
 description_directory = "supplier-data/descriptions"
 ip = ""
 
-def parse_text_file(file_dir):
+def parse_text_file(file_dir,filename):
     with open(file_dir, "r") as file:
         result = {}
         result["name"] = file.readline()
         result["weight"] = int(file.readline().split(" ")[0])
         result["description"] = file.readline()
-        result["image_name"] = file.readline()
+        result["image_name"] = filename.split(".")[0] + ".jpeg"
         return result
 
 
 def __main__():
     for file in os.listdir(description_directory):
-        data = parse_text_file(description_directory+"/"+file)
+        data = parse_text_file(description_directory+"/"+file,file)
 
         response = requests.post(ip+"/fruits",data)
         print("parsed file {} uploading to {}/fruits, responded with {}".format(file,ip,response.status_code))
