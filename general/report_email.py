458ad6968a72184485ef1bbb8654c8da8a4bc3bb
KevinW <bird831205@hotmail.com>
2020-04-21 15:57:33 -0400

    sends email

--- general/report_email.py
+++ general/report_email.py
@@ -1,14 +1,39 @@
 import general.reports as reports
 import os
 import datetime
+import email.message
+import mimetypes
+import smtplib
 
 
-def generate_email():
-    pass
+def generate_email(sender, recipient, subject, body, attachment_path):
+    """Creates an email with an attachement."""
+    # Basic Email formatting
+    message = email.message.EmailMessage()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
+    message.set_content(body)
+
+    # Process the attachment and add it to the email
+    attachment_filename = os.path.basename(attachment_path)
+    mime_type, _ = mimetypes.guess_type(attachment_path)
+    mime_type, mime_subtype = mime_type.split('/', 1)
+
+    with open(attachment_path, 'rb') as ap:
+        message.add_attachment(ap.read(),
+                               maintype=mime_type,
+                               subtype=mime_subtype,
+                               filename=attachment_filename)
+
+    return message
 
 
-def send_email():
-    pass
+def send_email(message):
+    """Sends the message to the configured SMTP server."""
+    mail_server = smtplib.SMTP('localhost')
+    mail_server.send_message(message)
+    mail_server.quit()
 
 
 def get_paragraph():
@@ -24,10 +49,11 @@ def get_paragraph():
 
 def main():
     paragraph = get_paragraph()
-    print(paragraph)
-    # reports.generate_report("/tmp/processed.pdf",
-    #                         "Processed Update On {}".format(datetime.datetime.today().strftime('%B %d, %Y')),
-    #                         paragraph)
+    reports.generate_report("/tmp/processed.pdf",
+                            "Processed Update On {}".format(datetime.datetime.today().strftime('%B %d, %Y')),
+                            paragraph)
+    message = generate_email("automation@example.com","student-03-7c6da43177bf@example.com","Upload Completed - Online Fruit Store","All fruits are uploaded to our website successfully. A detailed list is attached to this email","/tmp/processed.pdf")
+    send_email(message)
 
 
 if __name__ == '__main__':
